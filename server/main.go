4f0922c6d53b40fb347065f61813482da384f596
SanjayKumar <0508sanjaykumar@gmail.com>
2025-05-03 23:01:17 +0530

    feat: Base updated commit

--- server/main.go
+++ server/main.go
@@ -1,8 +1,21 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/websocket"
+)
 
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request){
+
+}
 func main() {
-    fmt.Println("Hello, Go!")
+	http.HandleFunc("/ws",wsHandler)
+	log.Println("WebSocket server started on :8080")
 }
