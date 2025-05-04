2b04d7c78b18f4c8eb9b42406472bf91e9dbae99
SanjayKumar <0508sanjaykumar@gmail.com>
2025-05-04 15:01:01 +0530

    feat: Added page svelte for websocket connection

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    let ws: WebSocket | null = null;
+    let messages: string[] = []
+  
+    function connect() {
+      ws = new WebSocket("ws://localhost:8080/ws");
+  
+      ws.onmessage = (event) => {
+        messages = [...messages, `Received: ${event.data}`];
+      };
+  
+      ws.onopen = () => {
+        messages = [...messages, "Connected to WebSocket"];
+        ws.send("Hello from Svelte!");
+      };
+    }
+  </script>
+  
+  <button on:click={connect}>Connect to WebSocket</button>
+  
+  <ul>
+    {#each messages as msg}
+      <li>{msg}</li>
+    {/each}
+  </ul>
+  
