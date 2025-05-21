17a5613130edbe04930b89dd984a6fb41f7fb197
SanjayKumar <0508sanjaykumar@gmail.com>
2025-05-21 10:34:22 +0530

    feat: Updated Landing Page

--- __client__/src/pages/LandingPage.tsx
+++ __client__/src/pages/LandingPage.tsx
@@ -1,18 +1,22 @@
+import { motion } from "framer-motion"
+
+import { Button } from "@/components/ui/button"
+import { useTheme } from "@/components/theme-provider"
+
 import { Moon, Sun } from "lucide-react"
  
-import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { useTheme } from "@/components/theme-provider"
 
-const LandingPage = () => {
+function ModeToggle() {
   const { setTheme } = useTheme()
+ 
   return (
-     <DropdownMenu>
+    <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
@@ -27,11 +31,45 @@ const LandingPage = () => {
         <DropdownMenuItem onClick={() => setTheme("dark")}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
+  )
+}
+
+const LandingPage = () => {
+  return (
+    <div className="m-1.5">
+      <ModeToggle/>
+      <section className="min-h-screen flex flex-col justify-center items-center text-center px-4">
+        <motion.h1
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          className="text-5xl font-bold"
+          >
+          Welcome to PeerDrop 🚀
+        </motion.h1>
+
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.2, duration: 0.6 }}
+          className="text-xl text-muted-foreground mt-4 w-4xl"
+          >
+          PeerDrop is a secure, real-time peer-to-peer (P2P) file sharing and chat application built with modern technologies like React, Go, WebRTC, and Docker.
+        </motion.p>
+
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.4 }}
+          className="mt-6"
+          >
+          <Button>Get Started</Button>
+        </motion.div>
+
+      </section>
+    </div>
   )
 }
 
