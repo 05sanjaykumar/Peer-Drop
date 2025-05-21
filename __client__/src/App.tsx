90f443adb9ca9bed4ffaa46c3429abdf69babae9
SanjayKumar <0508sanjaykumar@gmail.com>
2025-05-21 10:20:18 +0530

    feat: Added Base setup for pages

--- __client__/src/App.tsx
+++ __client__/src/App.tsx
@@ -1,11 +1,15 @@
-
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
 
   return (
-    <>
-     <h1 className="text-blue-400">Hello world</h1>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </ThemeProvider>
   )
 }
 
