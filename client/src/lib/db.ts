e70ae8482e385a697c474ced3e327c3ed4cfd079
SanjayKumar <0508sanjaykumar@gmail.com>
2025-05-30 08:57:48 +0530

    feat: Updated Dexie

--- client/src/lib/db.ts
+++ client/src/lib/db.ts
@@ -7,15 +7,27 @@ export interface Profile {
   lastActive: Date;
 }
 
+export type Room = {
+  id: string;
+  name: string;
+  createdBy: string;
+  participants: string[];
+  createdAt: Date;
+};
+
+
 class WebRTCAppDB extends Dexie {
   profiles!: Table<Profile, string>;
+  rooms!: Table<Room, string>;
 
   constructor() {
     super("WebRTCAppDB");
-    this.version(1).stores({
+    this.version(2).stores({
       profiles: "id, name, lastActive",
+      rooms: "id, name, createdBy, createdAt"
     });
   }
 }
+
 
 export const db = new WebRTCAppDB();
